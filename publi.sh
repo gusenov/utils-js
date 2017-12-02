2ffec254523ecf240b916ad65e7eb5b6d71a6ed1
Аббас Гусенов <gusenov@live.ru>
2017-12-03 04:27:41 +0600

    0.1.6

--- publi.sh
+++ publi.sh
@@ -4,8 +4,8 @@ set -x # echo on
 npm run-script bundle
 
 git add .
-git commit -S -m "0.1.5"
-git tag -s v0.1.5 -m 'signed 0.1.5 tag'
+git commit -S -m "0.1.6"
+git tag -s v0.1.6 -m 'signed 0.1.6 tag'
 
 git push --force --tags com.github.gusenov.utils-js master:master
 
--- utils.js
+++ utils.js
@@ -398,6 +398,44 @@
     }
 
 }());
+/*jslint
+    devel: true,
+    node: true
+*/
+
+/*eslint-env
+    browser,
+    node
+*/
+
+/* eslint-disable
+    no-console
+*/
+
+(function () {
+    'use strict';
+
+    var RegExUtils = (function () {
+        function RegExUtils() { }
+        
+        /**
+         * @see {@link https://stackoverflow.com/questions/1707299/how-to-extract-a-string-using-javascript-regex|Stack Overflow}
+         */
+        RegExUtils.extractStringUsingJavaScriptRegEx = function (content, rx) {
+            var arr = content.match(rx) || [""]; // could also use null for empty value
+            return arr[0];
+        };
+        
+        return RegExUtils;
+    }());
+
+    if (typeof window === 'undefined') {
+        module.exports = RegExUtils;
+    } else {
+        window.RegExUtils = RegExUtils;
+    }
+
+}());
 /*jslint node: true */
 /*global
     window,
